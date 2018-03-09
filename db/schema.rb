66a2292bc3b186c97e2d0e194f2ee131f896dd98
github username <github email address>
2018-03-09 14:42:31 -0500

    model generate categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309035442) do
+ActiveRecord::Schema.define(version: 20180309193757) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
