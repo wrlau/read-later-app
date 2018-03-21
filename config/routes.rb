627cc32496001f1eb2f805254d274faafa16a11f
github username <github email address>
2018-03-21 11:54:09 -0400

    routing oauth flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :reading_lists
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
