3840f91db77ecf435ec6c0846629c699b2e00a92
github username <github email address>
2018-03-21 16:27:31 -0400

    add categories route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :reading_lists
   resources :articles
+  resources :categories, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
