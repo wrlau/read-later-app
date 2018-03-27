648c01f620c990d2d31213c3adf9401222b19ca5
github username <github email address>
2018-03-27 19:03:58 -0400

    update scraping css selectors

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,7 +1,16 @@
 class Scraper
-  #guardian uk website
-  #entries = div.fc-container__inner
-  #first heading = entries[0].css('.fc-container__header__title>a span').text OR entries[0].css('.fc-container__header__title').css('span')[0].text
 
-  #first link = entries[0].css('.fc-container__header__title>a')[0]['href']
+
+  #main page: doc = Nokogiri::HTML(open("https://www.theguardian.com/us"))
+  #categories: categories = doc.css('li.pillars__item')
+  #categories.length = 5 (news, opinion, sport, culture, lifestyle)
+  #first category = categories[0].css('a').text.strip
+  #first link = categories[0].css('a')[0]['href']
+
+  #this will print out each category and link
+  categories.each do |category|
+    puts category.css('a').text.strip
+    puts category.css('a')[0]['href']
+  end
+
 end
