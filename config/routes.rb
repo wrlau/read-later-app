e644096376e92ce954f23b6cc91b9256f65305aa
github username <github email address>
2018-03-31 21:04:30 -0400

    subcategory nested resource routing and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :reading_lists
   resources :articles
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:index, :show] do
+    resources :subcategories, only: [:show] 
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
