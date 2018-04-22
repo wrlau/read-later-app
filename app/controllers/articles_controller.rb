5e26d5bd0a34261f8fb3d597af7ed0b5d1620e9b
github username <github email address>
2018-04-21 18:40:51 -0700

    associate article with a new list in update form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,15 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    #binding.pry
-    reading_list = current_user.reading_lists.find(params[:article][:reading_list_id])
-    reading_list.articles << @article
+
+    if params[:article][:reading_list_attributes][:name]
+      reading_list = current_user.reading_lists.find_or_create_by(name: params[:article][:reading_list_attributes][:name])
+      reading_list.articles << @article
+    elsif params[:article][:reading_list_id]
+      reading_list = current_user.reading_lists.find(params[:article][:reading_list_id])
+      reading_list.articles << @article
+    end
+
     if @article.save
       redirect_to reading_list_path(reading_list)
     else
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :reading_list, optional: true
+  accepts_nested_attributes_for :reading_list
   belongs_to :category
   belongs_to :subcategory
 =begin
