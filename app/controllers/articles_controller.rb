faf0f29ae6aec53f8985f0b70036cacd3cf502af
github username <github email address>
2018-05-23 17:59:01 -0700

    article update fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
 
   def show
-    @article.build_reading_list
+    #@article.build_reading_list
   end
 
   def edit
