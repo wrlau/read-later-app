893e63b012009124a78e649b6bbd2f41668a70db
github username <wlau903@gmail.com>
2018-07-04 21:50:01 -0700

    Configure category and subcategory controllers response to json requests

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -4,6 +4,10 @@ class SubcategoriesController < ApplicationController
     @subcategory = Subcategory.find(params[:id])
     scrape_articles(@subcategory)
     @subcategory.articles
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @subcategory }
+    end
   end
 
 end
