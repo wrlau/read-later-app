4c75629109c7f51c9d6a54680959ecb7f682d459
github username <wlau903@gmail.com>
2018-07-06 23:46:53 -0700

    Article list appears on category show without redirect

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,9 +1,16 @@
-$(".js-more").on("click", function() {
+$(".js-more").on("click", function(event) {
+  alert("Stop right there!");
+  event.preventDefault();
   var categoryId = this.dataset.categoryId;
   var subcategoryId = this.dataset.subcategoryId;
   $.get("/categories/" + categoryId + "/subcategories/" + subcategoryId + ".json", function(data) {
-      debugger
-        console.log(data);
-        var articles = data; //returns an array
+      console.log(data);
+      var articles = data["articles"]; //returns an array of article objects, add in links to article serializer?
+      var newArticleList = "<ul>"
+      for (var i = 0; i < articles.length; i++) {
+        newArticleList += "<li>" + articles[i]["name"] + "</li>"
+      };
+      newArticleList += "</ul>"
+      $("#subcategory-" + subcategoryId).html(newArticleList);
       });
   });
