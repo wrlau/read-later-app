a697eb655d53f3eaf327c262748da697548a1211
github username <wlau903@gmail.com>
2018-08-04 16:14:46 -0700

    Create reading lists using ajax post request

--- app/assets/javascripts/reading_lists.js
+++ app/assets/javascripts/reading_lists.js
@@ -1,4 +1,35 @@
-$("button").on("click", function() {
-  debugger
-  alert("You submitted!!");
+$("button").on("click", function(event) {
+    event.preventDefault();
+    alert("You clicked!!");
+
+    var $button = $(this);
+    var url = $button.data("url");
+
+    //ajax call for loading create reading list partial
+    $.get(url, function(response) {
+      alert("you got the form!");
+
+      //parse string response (form header and actual form) into an array of DOM nodes
+      var html = $.parseHTML(response)
+      $button.after(html);
+
+      //isolate and render form
+      var form = html[2];
+      //hijack form submit event
+      $(form).submit(function(event) {
+        alert("you submitted the form!");
+        event.preventDefault();
+
+        var values = $(this).serialize();
+        //create new list
+        var posting = $.post(this.action, values);
+        //append new list onto the dom
+        posting.done(function(data) {
+          let newList = "<li><a href=" + `${this.url}/` + data["id"] + ">" + data["name"] + "</a></li>";
+          let getLists = $(".reading_lists");
+          getLists.append(newList);
+          $(html).html(""); //clear new reading list form
+        })
+      })
+    })
 })
