868f01f8dddfdc9b616199c8baadbd9150a229e6
wlau903 <wlau903@gmail.com>
2018-09-11 15:11:40 -0700

    Alphabetize categories index list on home page

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -14,18 +14,43 @@ $(".js-more").on("click", function(event) {
       });
   });
 
-  $("button").on("click", function(event) {
+  $("#start").on("click", function() {
     var $button = $(this);
     var url = $button.data("url");
 
     $.get(url, function(data) {
-      console.log(data);
-      debugger
       var newCategoryList = "<h3>Categories</h3><ul>"
       for (var i = 0; i < data.length; i++) {
         newCategoryList += "<li><a href="+ '/categories/' + data[i]["id"]+ ">"+ data[i]["name"] + "</a></li>"
       };
       newCategoryList += "</ul>"
-      $("button").after(newCategoryList);
-    });
+      $("#categoriesList").html(newCategoryList);
+    }, "json");
+  });
+
+  $("#alphabetize").on("click", function() {
+    var $alphabetize = $(this);
+    var url = $alphabetize.data("url");
+
+    $.get(url, function(data) { //returns array of category objects
+      //sort category objects in alphabetical order
+      var alphabetized = data.sort(function(a,b) {
+        var nameA = a.name.toUpperCase();
+        var nameB = b.name.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+      //iterate through categories objects, create links, and add to DOM
+      var newCategoryList = "<h3>Categories</h3><ul>"
+      for (var i = 0; i < alphabetized.length; i++) {
+        newCategoryList += "<li><a href="+ '/categories/' + alphabetized[i]["id"]+ ">"+ alphabetized[i]["name"] + "</a></li>"
+      };
+      newCategoryList += "</ul>"
+      $("#categoriesList").html(newCategoryList);
+    }, "json");
   });
